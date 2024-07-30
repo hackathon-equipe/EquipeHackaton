f877ae058c07db4f0b4186e0a7f5e6d9746efaee
vinyzada <vinyescolas@gmail.com>
2024-07-30 04:45:46 -0300

    feat: criado pagina de cada projeto #18

---
+++ src/views/projectsViews/projectFabricaClassView.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+import DefaultNavbar from '@/components/navbar/DefaultNavbar.vue'
+import DefaultTerminal from '@/components/terminal/DefaultTerminal.vue'
+import ButtonHelp from '@/components/help/ButtonHelp.vue'
+import HelpArea from '@/components/help/HelpArea.vue'
+const openHelpArea = ref(false)
+function openArea() {
+  openHelpArea.value = !openHelpArea.value
+}
+const commands = {
+  info: '/',
+}
+</script>
+<template>
+  <div class="tela">
+    <!-- Menu navegavel -->
+    <DefaultNavbar />
+    <main>
+      <!-- Prompt de comando -->
+      <DefaultTerminal :comando="commands" membroId="5" />
+      <!-- Area de ajuda -->
+      <ButtonHelp @openArea="openArea" />
+    </main>
+    <HelpArea v-if="openHelpArea" @openArea="openArea" />
+  </div>
+</template>
+<style scoped>
+.tela {
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+}
+.tela main {
+  display: flex;
+  flex-direction: column;
+  height: 70%;
+  justify-content: space-between;
+}
+</style>
