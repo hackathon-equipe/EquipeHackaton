6637e66e3a3663666b261241245d45cb8a573fb2
isabellirosa <isabelli.ifc@gmail.com>
2024-08-01 22:06:59 -0300

    FIX: Arrumando pagina selecionada no navbar #49

--- src/components/navbar/LinksNavbar.vue
+++ src/components/navbar/LinksNavbar.vue
@@ -1,18 +1,25 @@
 <script setup>
-import { RouterLink } from 'vue-router';
+import { RouterLink } from 'vue-router'
+import { useRoute } from 'vue-router'
+const route = useRoute()
 defineProps(['title', 'link'])
 </script>
 <template>
-    <RouterLink  class="router-link" :to="link">
-        <li class="color-gray" :class="[$route.fullPath==link? 'active':'']">{{ title }}</li>
+    <RouterLink class="router-link" :to="link">
+        <li class="color-gray" :class="[
+            $route.fullPath == link || $route.fullPath == link + '/' + route.params.id ? 'active' : ''
+        ]">
+            {{ title }}
+        </li>
     </RouterLink>
 </template>
 <style scoped>
-li{
+li {
     cursor: pointer;
 }
-.active{
+
+.active {
     font-weight: 700;
-    border-bottom: 2px solid #09548D;
+    border-bottom: 2px solid #09548d;
 }
 </style>
